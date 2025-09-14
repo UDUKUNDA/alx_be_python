e298402b58f10c97fb7fd9c9b96c5ac24bb2e63f
UDUKUNDA <udukundaelysee@gmail.com>
2025-09-14 19:32:53 +0200

    fix Simple Interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-P = 1000
-R = 0.05
-T = 3
-I = P * R * T
+Principal = 1000
+Rate = 0.05
+Time = 3
+Interest = Principal * Rate * Time
 
-print("The simple interest is: ", I)
+print("The simple interest is: ", Interest)
